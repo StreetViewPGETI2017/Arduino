3a3beb1cc03638209b9436385a8ecfb3380ed919
Krzysztof Dudziak <krz.dudziak@gmail.com>
2017-05-19 16:20:55 +0200

    Lekkie porządki

--- Arduino_Base/Arduino_Base.ino
+++ Arduino_Base/Arduino_Base.ino
@@ -29,12 +29,6 @@
 #define steps 32   
 #define moveSteps 32 * 64  //2048  
 
-
-/*You should have received a copy of the GNU Lesser General Public License along
-with MinIMU-9-Arduino-AHRS. If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
 // Uncomment the following line to use a MinIMU-9 v5 or AltIMU-10 v5. Leave commented for older IMUs (up through v4).
 //#define IMU_V5
 
@@ -209,44 +203,6 @@ int previousStateEncR = LOW;
   double gearRatio = 75.0; // the gear ratio of the motor is 75:1*/
 
 int servoTurn = 5;
-
-/*void readIMU() {
-  if ((millis() - timer) >= 20) // Main loop runs at 50Hz
-  {
-    counter++;
-    timer_old = timer;
-    timer = millis();
-    if (timer > timer_old)
-    {
-      G_Dt = (timer - timer_old) / 1000.0; // Real time of loop run. We use this on the DCM algorithm (gyro integration time)
-      if (G_Dt > 0.2)
-        G_Dt = 0; // ignore integration times over 200 ms
-    }
-    else
-      G_Dt = 0;
-
-    // *** DCM algorithm
-    // Data adquisition
-    Read_Gyro();   // This read gyro data
-    Read_Accel();     // Read I2C accelerometer
-
-    if (counter > 5)  // Read compass data at 10Hz... (5 loop runs)
-    {
-      counter = 0;
-      Read_Compass();    // Read I2C magnetometer
-      Compass_Heading(); // Calculate magnetic heading
-    }
-
-    // Calculations...
-    Matrix_update();
-    Normalize();
-    Drift_correction();
-    Euler_angles();
-    // ***
-    //for (int i = 0; i < 100; ++i)
-    //printdata();
-  }
-}*/
 
 long* readSonicData() {
   //long forwardDistance, leftDistance, rightDistance;
@@ -544,45 +500,6 @@ float rotateCamera(int cameraDirection,int camSpeed) //rotates camera
     stepper.step(waitingSteps);
     //delay(4000);
 
-  /*for (int i = 0; i < 10; ++i)
-    {
-    cameraServo.write(97);
-    //waitForRaspberry();
-    delay(500);
-    cameraServo.write(95.5);
-    for (int j = 0; j <20; ++j)
-    {
-        readIMU();
-        printdata();
-    }
-    printdata();
-    delay(500);
-    }
-    for (int i = 0; i < 10; ++i)
-    {
-    cameraServo.write(94);
-    //waitForRaspberry();
-    delay(500);
-    cameraServo.write(95.5);
-    for (int j = 0; j <20; ++j)
-    {
-        readIMU();
-        printdata();
-    }
-    delay(500);
-    }*/
-  //cameraStep = 1.5;
-  //SerialUSB.println(cameraDirection);
-  /*cameraServo.write(95 + cameraDirection * 1.5);
-  //waitForRaspberry();
-  delay(500 + (-1 + cameraDirection) * 50);
-  cameraServo.write(95);
-  for (int j = 0; j < 20; ++j)
-  {
-    readIMU();
-    printdata();
-  }
-  delay(500);*/
   return 22.5;
 }
 
@@ -637,55 +554,11 @@ void setup() {
 
   initIMU();
 
-  /*//Serial.begin(115200);
-  pinMode (STATUS_LED, OUTPUT); // Status LED
-
-  //SerialUSB.println("test1");
-  I2C_Init();
-  //SerialUSB.println("test2");
-
-  digitalWrite(STATUS_LED, LOW);
-  delay(1500);
-
-  /*Accel_Init();
-  Compass_Init();
-  Gyro_Init();
-  //SerialUSB.println("test3");
-  delay(20);
-
-  for (int i = 0; i < 32; i++) // We take some readings...
-  {
-    Read_Gyro();
-    Read_Accel();
-    //SerialUSB.println("test4");
-    for (int y = 0; y < 6; y++) // Cumulate values
-      AN_OFFSET[y] += AN[y];
-    delay(20);
-  }
-
-  for (int y = 0; y < 6; y++)
-    AN_OFFSET[y] = AN_OFFSET[y] / 32;
-
-  AN_OFFSET[5] -= GRAVITY * SENSOR_SIGN[5];
-
-  //Serial.println("Offset:");
-  for (int y = 0; y < 6; y++)
-    SerialUSB.println(AN_OFFSET[y]);
-
-  delay(2000);
-  digitalWrite(STATUS_LED, HIGH);
-
-  timer = millis();
-  delay(20);
-  counter = 0;*/
-  //your own initializattion code
+  
 }
 
 void loop() {
-  /*for ( int i = 0; i < 20; ++i)
-  {
-    readIMU();
-  }*/
+  
   //readIMU();
   if (SerialUSB.available()) recieveUSB(); //call event function
   if (fromUSB.received) {
